a95e25af8ebcdb0019312893a158f63c3ea61968
Anton <anto0784@elev.techcollege.dk>
2026-09-22 19:44:56 +0200

    i did an update

--- src/GlobalStyles/breakpoints.ts
+++ src/GlobalStyles/breakpoints.ts
@@ -1,6 +1,4 @@
-// Fælles breakpoints, så alle komponenter bruger de samme skæringspunkter.
-// Brug dem i styled-components sådan her:
-//   @media (max-width: ${breakpoints.tablet}) { ... }
+
 export const breakpoints = {
   mobile: "480px",
   tablet: "768px",
