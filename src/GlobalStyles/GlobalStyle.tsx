60a8a4fb5845dbbb39397f8de71b4c0a43f50eb8
Anton <anto0784@elev.techcollege.dk>
2026-09-24 20:34:35 +0200

    i added the right footer view

--- src/GlobalStyles/GlobalStyle.tsx
+++ src/GlobalStyles/GlobalStyle.tsx
@@ -2,25 +2,23 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap");
+
   html, body, #root {
     height: 100%;
   }
 
   body {
     margin: 0;
-
-    min-height: 130vh;
-
-    background-color: #4a2c17; /* the brown */
+    background-color: #4a2c17;
   }
 
   #root {
-    min-height: 150vh;¨
-
+    min-height: 150vh;
     max-width: 1650px;
-
     margin: 0 auto;
-    
     background: white;
+    display: flex;
+    flex-direction: column;
   }
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,68 +1,111 @@
 import styled from "styled-components";
+import { FaPinterest, FaInstagram, FaFacebookSquare, FaTwitterSquare } from "react-icons/fa";
 import { breakpoints } from "../../GlobalStyles/breakpoints";
+import footerBg from "../../assets/pngtree-newspaper-textured-background-with-grunge-and-vintage-elements-in-a-white-image_17281530.jpg"
 
+
+//jeg ved godt det er et langt import navn til billedet !:))
 const Footer = styled.footer`
-  background-color: #fcfcfc;
-  color: #000;
-  padding: 10px 22px;
-  margin-top: 2rem;
-  border-top: 1px solid #ddd;
+  font-family: "Open Sans", Arial, sans-serif;
+
+  background-image:
+
+    linear-gradient(90deg, rgba(255,255,255,0.95) 0%, rgba(255,255,255,0.85) 60%, rgba(255,255,255,0.4) 100%),
+    
+    url(${footerBg});
+
+
+  background-size: cover;
+
+
+  background-position: center;
+
+  color: #333;
+
+  padding: 15px 40px;
+
+  margin-top: auto;
 `;
 
-// Selve stylingen af footeren
-
 const FooterContent = styled.div`
+
   display: grid;
-  grid-template-columns: 120px 150px;
-  column-gap: 25px;
+
+  grid-template-columns: 255px 1fr auto;
 
   align-items: start;
 
   @media (max-width: ${breakpoints.mobile}) {
+
     grid-template-columns: 1fr;
+
     row-gap: 10px;
   }
 `;
 
-// Styling af de to kolonner, som indeholder virksomhedens data
-
 const FooterColumn = styled.div`
+
   display: flex;
+
   flex-direction: column;
 `;
 
-const CompanyName = styled.h2`
+const CompanyName = styled.h1`
+
   margin: 0 0 4px;
-  color: #b9873c;
-  font-size: 13px;
+
+  color: #d4694c;
+
+  font-size: 16px;
+
+  font-weight: 700;
 `;
 
 const Info = styled.p`
-  margin: 1px 0;
+
+  margin: 2px 0;
+
   line-height: 1.3;
-  font-size: 9px;
-  color: #000;
+
+  font-size: 15px;
+
+  color: #333;
+`;
+
+const Social = styled.div`
+
+  display: flex;
+
+
+  gap: 4px;
+
+  font-size: 30px;
+
+  color: #999;
 `;
 
 function Footeren() {
   return (
     <Footer>
       <FooterContent>
-
-        {/* Venstre kolonne: navn og adresse */}
         <FooterColumn>
           <CompanyName>WALLYWOOD</CompanyName>
           <Info>Øster Uttrupvej 1</Info>
           <Info>9000 Aalborg</Info>
         </FooterColumn>
 
-        {/* Højre kolonne: kontaktinformation */}
         <FooterColumn>
           <Info>CVR: 12345678</Info>
           <Info>MAIL: info@wallywood.dk</Info>
           <Info>MOBIL: +45 9812 3456</Info>
         </FooterColumn>
 
+        <Social>
+          <FaPinterest />
+          <FaInstagram />
+          <FaFacebookSquare />
+          <FaTwitterSquare />
+        </Social>
       </FooterContent>
     </Footer>
   );
